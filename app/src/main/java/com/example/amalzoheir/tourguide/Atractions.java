1002bd8ca7133b61b1aecd10834133b58b10ee1f
AZamalzoheir <A.Zamalzoheir95@gamil.com>
2017-11-17 22:44:10 +0200

    thirdcommit

--- app/src/main/java/com/example/amalzoheir/tourguide/Atractions.java
+++ app/src/main/java/com/example/amalzoheir/tourguide/Atractions.java
@@ -14,9 +14,8 @@ public class Atractions extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.text_list);
         ArrayList<TextGuide> textGuide = new ArrayList<TextGuide>();
-        textGuide.add(new TextGuide("Cairo", "City of a Thousand Minarets", R.drawable.cairo));
+        textGuide.add(new TextGuide("cairo", "City of a Thousand Minarets", R.drawable.cairo));
         textGuide.add(new TextGuide("Luxor", "Historic sites,Museums,Shopping", R.drawable.luxor));
-        textGuide.add(new TextGuide("Sharm El Sheikh", "Nature,Shopping", R.drawable.sharm));
         textGuide.add(new TextGuide("Giza Governorate", "Pyramids", R.drawable.giza));
         textGuide.add(new TextGuide("Red Sea and Sinai", " natural and archaeological landmarks", R.drawable.redsea));
         TextAdapter itemAdapter = new TextAdapter(this, textGuide, R.color.attraction);
--- app/src/main/java/com/example/amalzoheir/tourguide/Restrant.java
+++ app/src/main/java/com/example/amalzoheir/tourguide/Restrant.java
@@ -15,7 +15,7 @@ public class Restrant extends AppCompatActivity {
         ArrayList<TextGuide> textGuide = new ArrayList<TextGuide>();
         textGuide.add(new TextGuide("Nagguib Mahfouz Cafe", "5 El Baddistan Lane| Khan el_Khalili", R.drawable.naghib));
         textGuide.add(new TextGuide("Felfela", "15 Sharia Hoda Shaarawi Street", R.drawable.falafel));
-        textGuide.add(new TextGuide("Esplanade Cafe Restaurant", "El Kahattab InterContinental Cairo Citystars | omar ibn", R.drawable.esplanade));
+        textGuide.add(new TextGuide("Esplanade Cafe Restaurant", "El Kahattab InterContinental Cairo Citystars | omar ibn", R.drawable.esplande));
         textGuide.add(new TextGuide("El Fishawi", "Midan Hussien|Cairo", R.drawable.elfishawy));
         textGuide.add(new TextGuide("Koshary Abou Tarek", "Choppin In | Champollion", R.drawable.koshary));
         TextAdapter itemAdapter = new TextAdapter(this, textGuide, R.color.resturant);
